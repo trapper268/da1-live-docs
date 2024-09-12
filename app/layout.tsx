6d6945394622592bbba209f4fb9dfe9ad231e25f
huytt268 <tuanhuytruong35@gmail.com>
2024-09-12 16:21:27 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import { Inter as FontSants } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import './globals.css';
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const fontSants = FontSants({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go to collaborative editor"
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorBackground: "#060d18",
+          fontSize: '16px'
+        },
+
+      }
+      }>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased flex flex-col",
+            fontSants.variable
+          )}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
