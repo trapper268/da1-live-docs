f91a4f4e26e74320c671ea6f82323165859065c3
huytt268 <tuanhuytruong35@gmail.com>
2024-09-20 14:41:19 +0700

    feat: add user mention feature

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,17 +1,29 @@
 'use client'
 
 import Loader from '@/components/Loader'
-import { getClerkUsers } from '@/lib/actions/user.action'
+import { getClerkUsers, getDucumentUsers } from '@/lib/actions/user.action'
+import { useUser } from '@clerk/nextjs'
 import { ClientSideSuspense, LiveblocksProvider, RoomProvider } from '@liveblocks/react/suspense'
 import { ReactNode } from 'react'
 
 const Provider = ({ children }: { children: ReactNode }) => {
+    const { user: clerkUser } = useUser()
+
     return (
         <LiveblocksProvider
             authEndpoint="/api/liveblocks-auth"
             resolveUsers={async ({ userIds }) => {
                 const users = await getClerkUsers({ userIds })
                 return users
+            }}
+            resolveMentionSuggestions={async ({ text, roomId }) => {
+                const roomUsers = await getDucumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+                    text,
+                })
+
+                return roomUsers
             }}
         >
 
